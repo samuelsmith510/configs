2bd00c677222b90107b8623524e32294284d2e3d
Sam Smith <samuel.smith510@gmail.com>
2020-01-28 18:58:59 -0500

    Added automatic install of emacs packages and other items

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+(package-initialize)
+
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
@@ -6,12 +8,26 @@
 (setq ring-bell-function 'ignore)
 (setq inhibit-startup-screen 1)
 
-(require 'package)
-(add-to-list 'package-archives
-    '("MELPA" .
-      "http://melpa.milkbox.net/packages/"))
-(package-initialize)
+; first, declare repositories
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")))
 
+;; Init the package facility
+(require 'package)
+(package-initialize)
+(package-refresh-contents)
+
+;; Declare packages
+(setq my-packages
+      '(magit
+	smex
+        json-mode))
+
+;; Iterate on packages and install missing ones
+(dolist (pkg my-packages)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
 
 ;; Font size and preference
 (set-frame-font "Source Code Pro 12" nil t)
@@ -59,26 +75,12 @@
 
 ;; Column width
 (setq-default fill-column 80)
-
-
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(grep-command "grep --color=auto -rin ")
- '(grep-find-command
-   (quote
-    ("find . -type f -exec grep --color=auto -rin  /dev/null \\{\\} +" . 45)))
- '(grep-use-null-device nil)
- '(grep-use-null-filename-separator nil)
- '(tramp-use-ssh-controlmaster-options nil nil (tramp))
- '(tramp-verbose 6 nil (tramp)))
+ '(package-selected-packages (quote (smex magit json-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
